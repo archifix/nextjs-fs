ec1a1337cfcaad8c6123e35d87fde95a1833daea
ArchiFix <i.dontsov61rus@yandex.ru>
2023-10-04 21:14:17 +0300

    Update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,22 @@
 import Image from 'next/image'
+import Button from '@/components/Button/Button'
+import styles from './page.module.css'
+import Hero from 'public/hero.png'
 
 export default function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  )
+	return (
+		<main className={styles.container}>
+			<div className={styles.item}>
+				<h1 className={styles.title}>The Future of AI in the next few years</h1>
+				<p className={styles.desc}>
+					Turning your Idea into Reality. We bring together the teams from the
+					global tech industry.
+				</p>
+				<Button url='/portfolio' text='See Our Works' />
+			</div>
+			<div className={styles.item}>
+				<Image src={Hero} alt='my photo' className={styles.img} />
+			</div>
+		</main>
+	)
 }
