4c5c51deee6d4caca59179b2a4033915d3f1f1e9
ArchiFix <i.dontsov61rus@yandex.ru>
2023-10-04 19:40:51 +0300

    Update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
-  return (
-    <footer>
-      <div>2023 MyApp. All rights reserved</div>
-      <div></div>
-    </footer>
-  )
+	return (
+		<footer className={styles.container}>
+			<div>2023 MyApp. All rights reserved</div>
+			<div className={styles.social}>
+				<Image
+					src='/vk.png'
+					width={15}
+					height={15}
+					className={styles.icon}
+					alt='vk.com'
+				/>
+				<Image
+					src='/inst.png'
+					width={15}
+					height={15}
+					className={styles.icon}
+					alt='instagram'
+				/>
+				<Image
+					src='/twitter.png'
+					width={15}
+					height={15}
+					className={styles.icon}
+					alt='twitter'
+				/>
+				<Image
+					src='/yt.png'
+					width={15}
+					height={15}
+					className={styles.icon}
+					alt='youtube'
+				/>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,49 +1,55 @@
-import Link from "next/link"
+import Link from 'next/link'
+import styles from './page.module.css'
 
 const links = [
-  {
-    id: 1,
-    title: "Home",
-    url: "/",
-  },
-  {
-    id: 2,
-    title: "Portfolio",
-    url: "/portfolio",
-  },
-  {
-    id: 3,
-    title: "Blog",
-    url: "/blog",
-  },
-  {
-    id: 4,
-    title: "About",
-    url: "/about",
-  },
-  {
-    id: 5,
-    title: "Contact",
-    url: "/contact",
-  },
-  {
-    id: 6,
-    title: "Dashboard",
-    url: "/dashboard",
-  },
-];
+	{
+		id: 1,
+		title: 'Home',
+		url: '/',
+	},
+	{
+		id: 2,
+		title: 'Portfolio',
+		url: '/portfolio',
+	},
+	{
+		id: 3,
+		title: 'Blog',
+		url: '/blog',
+	},
+	{
+		id: 4,
+		title: 'About',
+		url: '/about',
+	},
+	{
+		id: 5,
+		title: 'Contact',
+		url: '/contact',
+	},
+	{
+		id: 6,
+		title: 'Dashboard',
+		url: '/dashboard',
+	},
+]
 
 const Navbar = () => {
-  return (
-    <nav>
-      <Link href='/'>MyAPP</Link>
-      <div>
-        {links.map((link) => (
-          <Link key={link.id} href={link.url}>{link.title}</Link>
-        ))}
-      </div>
-    </nav>
-  )
+	return (
+		<nav className={styles.container}>
+			<Link href='/' className={styles.logo}>
+				MyApp
+			</Link>
+			<div className={styles.links}>
+				{links.map(link => (
+					<Link key={link.id} href={link.url}>
+						{link.title}
+					</Link>
+				))}
+				<button className={styles.logout}>Logout</button>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
